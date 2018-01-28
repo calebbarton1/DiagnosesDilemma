7a28552801d41a9449e05430ee35105069b02ac1
Caleb Barton <caleb.barton@hotmail.com>
2018-01-28 14:52:45 +1000

    #whoneedscommitnotes

--- DiagnosesDilemma/Assets/Scripts/DiseaseManager.cs
+++ DiagnosesDilemma/Assets/Scripts/DiseaseManager.cs
@@ -21,6 +21,19 @@ public class DiseaseManager : MonoBehaviour
 
     public GameObject water;
 
+    public GameObject newspaper;
+    public Text newspaperText;
+
+    public AudioClip success;
+    public AudioClip fail;
+    private AudioSource audsource;
+
+    public Transform otherCamTarget;
+    public Camera otherCam;
+    public GameObject disableCanv;
+
+    public GameObject targetBlocker;
+
     #region TimerStuff
     private double timer;
     public double startTimer;
@@ -29,6 +42,8 @@ public class DiseaseManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        audsource = GetComponent<AudioSource>();
+        newspaper.SetActive(false);
         timer = startTimer;
         disease = GameObject.FindGameObjectWithTag("disease");
         fx = disease.GetComponent<DiseaseFX>();
@@ -41,9 +56,10 @@ public class DiseaseManager : MonoBehaviour
     {
         UpdateTimer();
 
-        if (testCount > 2)
+        if (testCount >= 2)
         {
-            //endgame
+            EnableNewspaper("IDIOT SCIENTIST KILLS EVERYONE!");
+            audsource.PlayOneShot(fail);
         }
     }
 
@@ -54,12 +70,19 @@ public class DiseaseManager : MonoBehaviour
 
         else
         {
-            //end game?
+            EnableNewspaper("NO CURE FOR PLAGUE FOUND. END OF HUMANITY!");            
         }
 
         TimeSpan time = TimeSpan.FromSeconds(timer);
         timerText.text = string.Format("Time remaining till outbreak:\n {0}m:{1}s", time.Minutes, time.Seconds);
 
+    }
+
+    private void EnableNewspaper(string _text)
+    {
+        newspaper.SetActive(true);
+        newspaperText.text = _text;
+        targetBlocker.SetActive(true);
     }
 
     private void ChangeWaterColor()
@@ -69,61 +92,84 @@ public class DiseaseManager : MonoBehaviour
 
     public void TestTransmission(int _buttonNum)
     {
-        ++testCount;
-        if (_buttonNum == diseaseHolder.currDisease.myTransmission.myPair.button)
+        if (!m_TransmissionSolved)
         {
-            Debug.LogFormat("Correct Transmission vector: {0}", diseaseHolder.currDisease.myTransmission.transmissionType);
-            m_TransmissionSolved = true;
-            //press down all buttons
-            //foreach(GameObject go in buttons)
-            //{
-            //    go.GetComponent<buttonPress>().DisableButt();
-            //}
-
-            for (int index = 0; index < buttons.Count-2; ++index)
+            if (_buttonNum == diseaseHolder.currDisease.myTransmission.myPair.button)
             {
-                buttons[index].GetComponent<buttonPress>().DisableButt();
+                Debug.LogFormat("Correct Transmission vector: {0}", diseaseHolder.currDisease.myTransmission.transmissionType);
+                m_TransmissionSolved = true;
+                //press down all buttons
+                //foreach(GameObject go in buttons)
+                //{
+                //    go.GetComponent<buttonPress>().DisableButt();
+                //}
+
+                for (int index = 0; index < buttons.Count - 2; ++index)
+                {
+                    buttons[index].GetComponent<buttonPress>().DisableButt();
+                }
+                buttons[_buttonNum - 1].GetComponent<MeshRenderer>().material.color = Color.yellow;
+            }
+
+            else
+            {
+                ++testCount;
+                //do bad thing
+                Debug.LogFormat("INCORRECT Transmission vector: {0}", diseaseHolder.currDisease.myTransmission.transmissionType);
+                StartCoroutine(fx.Shake(200, 0.01f, 0.3f));
+                buttons[_buttonNum - 1].GetComponent<buttonPress>().StartPress(0);
+                ChangeWaterColor();
             }
         }
-
-        else
-        {
-            //do bad thing
-            Debug.LogFormat("INCORRECT Transmission vector: {0}", diseaseHolder.currDisease.myTransmission.transmissionType);
-            StartCoroutine(fx.Shake(200, 0.01f, 0.3f));
-            buttons[_buttonNum-1].GetComponent<buttonPress>().StartPress(0);
-            ChangeWaterColor();
-        }
+        CheckAnswer();
     }
 
     public void TestStrain(int _buttonNum)
     {
-        ++testCount;
-        if (_buttonNum == diseaseHolder.currDisease.myStrain.button)
+        if (!m_StrainSolved)
         {
-            m_StrainSolved = true;
-            Debug.LogFormat("Correct Strain Value: {0}", diseaseHolder.currDisease.myStrain.strainType);
-            for (int index = 4; index < buttons.Count; ++index)
+            if (_buttonNum == diseaseHolder.currDisease.myStrain.button)
             {
-                buttons[index].GetComponent<buttonPress>().DisableButt();
+                m_StrainSolved = true;
+                Debug.LogFormat("Correct Strain Value: {0}", diseaseHolder.currDisease.myStrain.strainType);
+                for (int index = 4; index < buttons.Count; ++index)
+                {
+                    buttons[index].GetComponent<buttonPress>().DisableButt();
+                }
+
+                buttons[_buttonNum - 1].GetComponent<MeshRenderer>().material.color = Color.yellow;
+            }
+
+            else
+            {
+                //bad thing
+                ++testCount;
+
+                Debug.LogFormat("INCORRECT Strain Value: {0}", diseaseHolder.currDisease.myStrain.strainType);
+                StartCoroutine(fx.Shake(200, 0.01f, 0.3f));
+                buttons[_buttonNum - 1].GetComponent<buttonPress>().StartPress(0);
+                ChangeWaterColor();
             }
         }
-
-        else
-        {
-            //bad thing
-            Debug.LogFormat("INCORRECT Strain Value: {0}", diseaseHolder.currDisease.myStrain.strainType);
-            StartCoroutine(fx.Shake(200, 0.01f, 0.3f));
-            buttons[_buttonNum - 1].GetComponent<buttonPress>().StartPress(0);
-            ChangeWaterColor();
-        }
+        CheckAnswer();
     }
 
     private void CheckAnswer()
     {
         if (m_StrainSolved && m_TransmissionSolved)
         {
-            //good game
+            //EnableNewspaper("CURE FOUND! SCIENTIST GIVEN NOBEL PRIZE");
+            //move othercam down to show vial
+            //new buttons
+            //players select what they literally just pressed
+            otherCam.gameObject.transform.position = otherCamTarget.position;
+            disableCanv.SetActive(false);
         }
+    }
+
+    public void WinGame()
+    {
+        EnableNewspaper("CURE FOUND! SCIENTIST GIVEN NOBEL PRIZE");
+        audsource.PlayOneShot(success);
     }
 }
--- DiagnosesDilemma/Assets/Scripts/RandomRotate.cs
+++ DiagnosesDilemma/Assets/Scripts/RandomRotate.cs
@@ -6,11 +6,13 @@ public class RandomRotate : MonoBehaviour
 {
     Quaternion currRot;
     Quaternion targetRot;
+    DiseaseFX fx;
 
     // Use this for initialization
     void Start()
     {
         ChooseNewRotTarget();
+        fx = GetComponent<DiseaseFX>();
     }
 
     // Update is called once per frame
@@ -29,5 +31,12 @@ public class RandomRotate : MonoBehaviour
     {
         targetRot = Random.rotation;
         //Debug.Log("New rotation target: " + targetRot);
+    }
+
+    private void OnMouseDown()
+    {
+        ChooseNewRotTarget();
+        StartCoroutine(fx.Shake(70, 0.007f, 0.1f));
+        GetComponent<AudioSource>().Play();
     }
 }
